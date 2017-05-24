2f6755524aa5bbb21f528a67a0ee496e684433d9
javier.robes <javier.robes@alestis.aero>
2017-05-24 08:45:14 +0200

    ...ya va saliendo...

--- OP2-PARSER-02/src/TEST_BINARY_FILE.java
+++ OP2-PARSER-02/src/TEST_BINARY_FILE.java
@@ -7,15 +7,16 @@ import com.google.common.io.LittleEndianDataInputStream;
 
 public class TEST_BINARY_FILE {
 	
-	//static String fileName = "bancada-a320-conf-3100-sol101.op2";
+	static String fileName = "bancada-a320-conf-3100-sol101.op2";
 	//static String fileName = "tocho-interno-loop-03.op2";
-	static String fileName = "bdf-and-op2.op2";
+	//static String fileName = "bdf-and-op2.op2";
 
 	
 	private static LittleEndianDataInputStream fileInputStream = null;
 	static FileInputStream inputStream;
 	private static boolean finDeArchivo = false;
 	private static int valor;
+	private static String dataBlockName;
 	
 	
 	public static void main(String[] args) {
@@ -24,106 +25,85 @@ public class TEST_BINARY_FILE {
 		
 		
 		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		File f = new File(fileName);
 		System.out.println("TAMAÑO DEL ARCHIVO\t"+f.length());
 		fileInputStream = new LittleEndianDataInputStream(inputStream);
 		try {
-			
 			readOP2Header();
-			
 			while(!finDeArchivo){
-				
 				finDeArchivo = leeDataBlock();
-				//finDeArchivo = true;
 			}
-			
-			
-			
-	/*		
-			fileInputStream.skipBytes((int) (f.length()-64));
-			System.out.println(fileInputStream.readInt());//22272
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//-27
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//1
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//0
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//0
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//4
-			System.out.println(fileInputStream.readInt());//0
-			System.out.println(fileInputStream.readInt());//4
-*/
-			
-			
-			
 			fileInputStream.close();
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
 	}
-
-	
 	private static boolean leeDataBlock() {
 		boolean finDataBlock = false;
 		while(!finDataBlock){
 			valor = readTri(fileInputStream);
 			if(valor == 0)return true;
-			int i = 0;
 			finDataBlock = leeDataRecord(valor);
-			i++;
-			
-			
 		}
+		System.out.println("\n");
 		return false;
 	}
-
-
 	private static boolean leeDataRecord(int valor2) {
 		System.out.print("Data Record\t"+valor2+"\t");
 		int numeroDeDatos;
 		if(valor2 == 2){
-			leeCabeceroDataBlock();
+			dataBlockName = leeCabeceroDataBlock();
 			return false;
 		}
 		else if(valor2 <0){
 			readTri(fileInputStream);//1
 			readTri(fileInputStream);//0
 			numeroDeDatos = readTri(fileInputStream);//4  num bytes*4  4
+			System.out.print(numeroDeDatos*4 +"\t");
 			if(numeroDeDatos == 0)return true;
-			procesarDatos(numeroDeDatos);
+			procesarDatos(valor2, numeroDeDatos, dataBlockName);
 			return false;
-
 		}
 		else{
 			procesarDatos(valor2);
 			return false;
-
 		}
 	}
-
 
 	private static void procesarDatos(int numeroDeDatos) {
 		try {
 			fileInputStream.skipBytes(numeroDeDatos*4 + 8);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
 	}
 
+	private static void procesarDatos(int valor, int numeroDeDatos, String dataBlockName2) {
+		if(valor == -3 && dataBlockName2.startsWith("O")){
+			System.out.println("Es el principio de OFP table, dentro de la palabra de configuracion...");
+			
+			try {
+				fileInputStream.skipBytes(numeroDeDatos*4 + 8);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		else{
+			try {
+				fileInputStream.skipBytes(numeroDeDatos*4 + 8);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+	}
 
-	private static void leeCabeceroDataBlock() {
+	private static String leeCabeceroDataBlock() {
 		String cadena1 = null;
 		try {
 			fileInputStream.readInt();//8
@@ -135,9 +115,7 @@ public class TEST_BINARY_FILE {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}//4
-		//System.out.println("*******************************************");
-		//System.out.println("* DATA BLOCK:\t"+cadena1);
-		//System.out.println("*******************************************");
+		return cadena1;
 		
 	}
 
@@ -151,8 +129,6 @@ public class TEST_BINARY_FILE {
 				cadena.append((char)micadena[i]);
 				//System.out.println((char)micadena[i]);
 			}
-			//System.out.println("CHAR4 = " + cadena);
-			//myStream.readInt();
 		
 		} catch (IOException e) {
 			e.printStackTrace();
@@ -170,10 +146,8 @@ public class TEST_BINARY_FILE {
 			System.out.println("FECHA");
 			fileInputStream.skipBytes(8);//4 y 12
 			fileInputStream.read(myByteDate, 0, 12);
-				
 			System.out.println(myByteDate[0]+"/"+myByteDate[4]+"/"+myByteDate[8]);//Fecha5/7/15 (12 bytes)
 			System.out.println(fileInputStream.readInt());//12
-			
 			
 		} catch (IOException e) {
 			e.printStackTrace();
